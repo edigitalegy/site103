0008b09eb2895a08cca05e5772193ea15c9e6760
edigitalegy <130320276+edigitalegy@users.noreply.github.com>
2024-07-13 01:07:32 +0400

    Add files via upload

---
+++ lessons/js/point.js
@@ -0,0 +1,71 @@
+const tooltips = document.querySelectorAll('.all-tooltip .tooltip');
+const fullDiv = document.querySelector('section');
+const container = document.querySelector('.container')
+let timeoutId;
+window.addEventListener('resize', contentPosition);
+window.addEventListener('DOMContentLoaded', contentPosition);
+
+function contentPosition(){
+  tooltips.forEach(tooltip => {
+    const pin = tooltip.querySelector('.pin');
+    const content = tooltip.querySelector('.tooltip-content')
+    const arrow = tooltip.querySelector('.arrow');
+    // const pinLeft = pin.offsetLeft;
+  /*
+    if (pin.offsetLeft + content.offsetWidth / 2 > fullDiv.offsetWidth) {
+      const extraLeft = fullDiv.offsetWidth - (pin.offsetLeft + content.offsetWidth / 2);
+      // console.log('right-conflict', tooltip)
+      content.style.left = pin.offsetLeft - content.offsetWidth / 2 + extraLeft - 30+ 'px';
+      content.style.top = pin.offsetTop + -120 + 'px';
+    } else if (pin.offsetLeft + container.offsetLeft < content.offsetWidth / 2 ){
+      // console.log('left conflict', pin.offsetLeft)
+      content.style.left = - container.offsetLeft +'px';
+      content.style.top = pin.offsetTop + -120 + 'px';
+    } else {
+      content.style.left = pin.offsetLeft - content.offsetWidth / 2 + 'px';
+      content.style.top = pin.offsetTop + -120 + 'px';
+    }
+    arrow.style.left = pin.offsetLeft - content.offsetLeft + pin.offsetWidth/2 + 'px';
+*/
+  })
+}
+tooltips.forEach(tooltip => {
+  const pin = tooltip.querySelector('.pin');
+  const content = tooltip.querySelector('.tooltip-content');
+var pinp=1;
+
+
+  pin.addEventListener('click', () => {
+    if(pinp==2)
+    {
+      tooltip.classList.remove('active')
+      pinp=1;
+    }
+    else
+    {
+    tooltip.classList.add('active');
+    pinp=2;
+    }
+  })
+
+/*
+  pin.addEventListener('mouseleave', () => {
+    timeoutId = setTimeout( () => {
+      tooltip.classList.remove('active')
+    },1000)
+  })
+  content.addEventListener('mouseover', () => {
+    clearTimeout(timeoutId)
+    tooltip.classList.add('active');
+  })
+  content.addEventListener('mouseclick', () => {
+    clearTimeout(timeoutId)
+    tooltip.classList.add('active');
+  })
+  content.addEventListener('mouseleave', () => {
+    timeoutId = setTimeout(() => {
+      tooltip.classList.remove('active')
+    }, 1000)
+  })
+    */
+})
